955fdc15bcf14ad620cc82a5e09d18c08cd3128f
japheth <awesomejaphet@gmail.com>
2022-09-09 17:48:23 -0700

    puts function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,12 +5,13 @@
  *
  * Description: using the main function
  * This is a C program thnat prints "Programming is like building a multilingual puzzle"
- * Return(0)
+ * Return:0
  */
 
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+     puts("\"Programming is like building a multilingual puzzle");
+     return(0);
 }
