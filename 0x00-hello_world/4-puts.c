70e5cfdb4fda836c82a0f05026e42fea46a63cc2
japheth <awesomejaphet@gmail.com>
2022-09-09 17:40:32 -0700

    print using a puts fn

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - prints a string in put function
+ *
+ * Description: using the main function
+ * This is a C program thnat prints "Programming is like building a multilingual puzzle"
+ * Return(0)
+ */
+
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
