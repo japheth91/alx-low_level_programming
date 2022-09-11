3575d020aa15308ad55bc00c1d545074ec4d6d43
japheth <awesomejaphet@gmail.com>
2022-09-11 04:15:05 -0700

    task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,14 +10,17 @@
 int main(void)
 {
 	char ch;
+
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 		putchar(ch);
 	}
+
 	for (ch = 'A' ; ch <= 'Z' ; ch++)
 	{
 		putchar(ch);
 	}
+	
 	putchar('\n');
 	return (0);
 }
