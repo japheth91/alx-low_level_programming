43e36f3ccea1b384187d851d35daf1189376d141
japheth <awesomejaphet@gmail.com>
2022-09-11 07:50:49 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,13 +5,13 @@
  *  * main - prints all possible combinations of two two-digit numbers
  *
  *   * Return: 0
- *
  */
 
 int main(void)
 
 {
 	int i, j;
+
 	for (i = 0; i < 100; i++)
 	{
 		for (j = 0; j < 100; j++)
@@ -23,7 +23,7 @@ int main(void)
 				putchar(' ');
 				putchar((j / 10) + 48);
 				putchar((j % 10) + 48);
-			
+
 				if (i != 98 || j != 99)
 				{
 					putchar(',');
