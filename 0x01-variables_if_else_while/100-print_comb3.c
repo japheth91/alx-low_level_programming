86bca6e80fd2a80dcfa5188e28ef16c9cbebdc56
japheth <awesomejaphet@gmail.com>
2022-09-11 06:00:43 -0700

    task 10=

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - combination of numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = '0' ; a <= '9' ; a++)
+	{
+		for (b = '0' ; b <= '9' ; b++)
+		{
+			if (a < b)
+			{
+				putchar(a);
+				putchnar(b);
+
+				if (a != '8' || (a == '8' && b != '9'))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
