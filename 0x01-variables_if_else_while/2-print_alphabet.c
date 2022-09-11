611fa66a49002bfbd9d128508900ed2fbda24599
japheth <awesomejaphet@gmail.com>
2022-09-11 03:02:54 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints lower case alphagbets
+ *
+ * Description: using the main function
+ * this program prints "Programing Variables"
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= '2' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
