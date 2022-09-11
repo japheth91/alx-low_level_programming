57f9e8c61f7f1d3d383683debb87cb901f33a0df
japheth <awesomejaphet@gmail.com>
2022-09-11 07:20:52 -0700

    task 12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - combination of numbers
+ *
+ * Desription: Prints a description of numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int n1 = 0, n2;
+
+	while(n1 <= 99)
+	{
+		n2 = n1;
+		while(n2 <= 99)
+		{
+			if (n2 != n1)
+			{
+				putchar((n1 / 10) + 48);
+				putchar((n1 % 10) + 48);
+				putchar(' ');
+				putchar((n2 / 10) + 48);
+				putchar((n2 % 10) + 48);
+
+				if(n1 != 98 || n2 != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++n2;
+		}
+		++n1;
+	}
+	putchar('/n');
+	return 0;
+}
