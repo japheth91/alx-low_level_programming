e1c4b75a07420d4c9b2bd57b7f9cc9878ce8f44b
japheth <awesomejaphet@gmail.com>
2022-09-11 05:27:47 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Hexadecimal strings
+ *
+ * Description: Prints code in hexadecimal printing
+ * Return: 0
+ */
+int main(void)
+{
+char h;
+
+for (h = '0' ; h <= '9' ; h++)
+{
+	putchar(h);
+}
+for (h = 'a' ; h <= 'f' ; h++)
+{
+	putchar(h);
+}
+putchar('\n');
+return (0);
+
+}
