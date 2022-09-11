04e95b4ced1a47fe50e976602650040465745800
japheth <awesomejaphet@gmail.com>
2022-09-11 14:30:38 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,11 @@
 /**
  * main - prints a string in put function
  *
- * Description: using the main function
- * This is a C program thnat prints "Programming is like building a multilingual puzzle"
+ * Description:This is a C program thnat prints "Programming is like building a multilingual puzzle"
  * Return:0
  */
 int main(void)
 {
-		puts ("\"Programming is like building a multilingual puzzle");
-       		return (0);
+	puts ("\"Programming is like building a multilingual puzzle");
+       	return (0);
 }
