52b13786a4c88015488f79b27662f92e9b73688f
japheth <awesomejaphet@gmail.com>
2022-09-16 12:53:51 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,15 +3,12 @@
 /**
  * main - tests function that prints if integber is positive or negative
  * Return: 0
-:wq
 */
 
 int main(void)
 {
 int i;
-
 i = 98;
-
 positive_or_negative(i);
 
 return (0);
