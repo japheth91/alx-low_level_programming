b02976dcf59bb67f7779506b957be45679ac2651
japheth <awesomejaphet@gmail.com>
2022-09-19 14:35:40 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,11 +2,11 @@
 
 /**
  * reset_to_98 - resets the value of given integer to 98
- * @h: parameter
+ * @n: parameter
  * Return : nothing
  */
 
 void reset_to_98(int *n)
 {
-	*n = 98;
+	n = 98;
 }
