9c4182eede5057631a853ccb143e766f9a785da6
japheth <awesomejaphet@gmail.com>
2022-09-22 11:17:36 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,22 +8,16 @@
  */
 char *_strcat(char *dest, char *src);
 {
-	int a, b, i;
-	int len1 = 0;
-	int len2 = 0;
+	int len = 0, i;
 
-	for (a = 0; *(dest + a) != '\0'; a++)
-	{
-		len1++;
-	}
-	for (b = 0; *(src + b) != '\0'; b++)
-	{
-		len2++;
-	}
-	for (i = 0; i <= len2; i++)
-	{
-		*(dest + (len1 + i)) = *(src + 1);
-	}
+	while (dest[len])
+		len++;
 
+	for (i = 0; src[i] != 0; i++)
+	{
+		dest[len] = src[i];
+		len += 1;
+	}
+	dest[len] = '\0';
 	return (dest);
 }
