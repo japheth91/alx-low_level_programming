cb60be5ef87c7a4c758f3647d86a00a99ce79758
japheth <awesomejaphet@gmail.com>
2022-09-27 15:50:09 -0700

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,23 +1,49 @@
 #include "main.h"
+#include <stddef.h>
+#include <stdio.h>
 
 /**
- * _strpbrk - locates first occurence of any bytes from accept in s
+* _strpbrk - a function that searches a string for any of a set of bytes.
+* The function locates the first occurrence in the string s of any of the
+ * bytes in the string accept
+ * @s: The string to search
+ * @accept: the substring to find
  *
- * @s: string to work on
- * @accept: pattern to match against
- * Return: pointer to the first match
+ * Return: Returns a pointer to the byte in s that matches one of the bytes
+ * in accept, or NULL if no such byte is found
  */
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	char *nil = NULL;
+	int i;
+	char *ptr;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; ++i)
 	{
-		for (j = 0; accept[j] != '\0'; j++)
+		if (is_present(accept, *(s + i)))
 		{
-			if (s[i] == accept[j])
-				return (s + i);
+			ptr = &s[i];
+			return (ptr);
 		}
 	}
-	return (NULL);
+	return (nil);
+}
+
+/**
+ * is_present - a function that checks if a character is preset in a string
+ * @str: the string
+ * @a: the character
+ *
+ * Return: return unsigned int 1 if present, else return 0
+ */
+unsigned int is_present(char *str, char a)
+{
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; ++i)
+	{
+		if (a == *(str + i))
+			return (1);
+	}
+	return (0);
 }
