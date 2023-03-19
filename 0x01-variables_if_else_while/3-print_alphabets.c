2ab1d95645e8f6abd6ddad7a6fe433e5525c1a62
japheth91 <awesomejaphet@gmail.com>
2023-03-19 08:04:42 -0700

    3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	char i, j;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	for (j = 'A' ; j <= 'Z' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
