1e73f669f6a9d7033eccbd31d3713556215015c9
japheth91 <awesomejaphet@gmail.com>
2023-03-19 09:34:53 -0700

    9-print_comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,10 +9,14 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		putchar(' ');
-		putchar(i + '0');
-		putchar(',');
+		if (i == 9)
+			putchar(i + '0');
+		else
+		{
+			putchar(' ');
+			putchar(i + '0');
+			putchar(',');
+		}
 	}
-		putchar('\n');
 	return (0);
 }
