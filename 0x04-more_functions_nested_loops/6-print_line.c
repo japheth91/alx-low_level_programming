d9464b960d74836eb7b40dfac29452e5182ac0a4
japheth91 <awesomejaphet@gmail.com>
2023-03-25 16:45:14 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,16 +8,13 @@ void print_line(int n)
 {
 	int i = 0;
 
-	if (n < 0 && n == 0)
-		{
-			_putchar('\n');
-		}
-	else
+	if (n > 0)
 	{
-		for (i = 0 ; i <= n ; i++)
+		while (i < n)
 		{
-			_putchar('_');
+			_putchar(95);
+			i++;
 		}
-			_putchar('\n');
 	}
+		_putchar('\n');
 }
