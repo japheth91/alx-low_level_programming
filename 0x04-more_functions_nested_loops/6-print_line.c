2fbc8a8ec67f52286c34c43443c6bb0b5ae83bf4
japheth91 <awesomejaphet@gmail.com>
2023-03-25 16:10:49 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - prints _
+ * @n: function parameter
+ * Remain: 0 always
+ */
+void print_line(int n)
+{
+	int i = 0;
+
+	while (i <= n)
+	{
+		if (n <= 0)
+		{
+		_putchar('\n');
+		}
+		i++;
+		_putchar('_');
+	}
+		_putchar('\n');
+}
