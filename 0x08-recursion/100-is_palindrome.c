1f03fba5bfc6f0765e77a26dd4eae8b14342152e
japheth91 <awesomejaphet@gmail.com>
2023-04-06 17:51:30 -0700

    100-palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string
+ * Return: the length of the string
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
+/**
+ * comparingFunction - compares each character of the string
+ * @s: string
+ * @n1: smallest integer
+ * @n2: biggest integer
+ * Return: 1
+ */
+int comparingFunction(char *s, int n1, int n2)
+{
+	if (*(s + n1) == *(s + n2))
+	{
+		if (n1 == n2 || n1 == n2 +1)
+			return (1);
+		return (0 + comparingFunction(s, n1 + 1, n2 - 1));
+	}
+	return (0);
+}
+/**
+ * is_palindrome - detects if the string is a palindrome
+ * @s: string
+ * Return: 1 for success 0, for failure
+ */
+int is_palindrome(char *s)
+{
+	if (*s == '\0')
+		return (1);
+	return (comparingFunction(s, 0, _strlen_recursion(s) - 1));
+}
