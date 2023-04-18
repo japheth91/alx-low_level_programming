65019eafbe1eabab44f45e25b0a30e86ab92efce
japheth91 <awesomejaphet@gmail.com>
2023-04-18 14:09:13 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: array of arguments
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
+
