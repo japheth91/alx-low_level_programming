95da26579eb5a2ca047028d1fa1ca88cc078badd
japheth91 <awesomejaphet@gmail.com>
2023-04-17 17:24:37 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct function of a dog
+ * @name: function paramter
+ * @age: function parameter
+ * @owner: function parameter
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
